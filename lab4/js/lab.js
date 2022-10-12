d67fb41f7037c37edd4697699aff1c1596a39008
Levi <lmorganw@ucsc.edu>
2022-10-12 13:29:30 -0700

    10/12/2022

---
+++ lab4/js/lab.js
@@ -0,0 +1,8 @@
+ * @author Levi M-W.
+//create a 10x10 square of boxes.
+  //Establish player colors
+    //Accept user input each turn
+      //Place associated color line between dots
+        //Make computer search for possible plays to create boxes
+      //Play turn to maximize points earned
+    //Count players points/boxes
