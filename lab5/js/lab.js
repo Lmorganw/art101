1ddf0f43663fbe391b511d9f628cc3023886c2dd
Levi <lmorganw@ucsc.edu>
2022-10-17 23:07:40 -0700

    10/17

---
+++ lab5/js/lab.js
@@ -0,0 +1,19 @@
+/*
+ * Author:    Levi
+ * Created:   10.17.2022
+ */
+ // define variables
+ make = "Suzuki"
+ model = "DRZ-400"
+ color = "Yellow"
+ year = 2019;
+
+ //calculate
+ age = 2022 - year;
+
+ //output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "Year<br>");
+ document.writeln("Age: " + age + " Year<br>");
