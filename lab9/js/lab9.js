00e63851add39d1b565876cbf7cb4402abdcb121
Levi <lmorganw@ucsc.edu>
2022-11-01 16:57:42 -0700

    11/1

---
+++ lab9/js/lab9.js
@@ -0,0 +1,20 @@
+/*
+* author: Levi
+* Created: 10.21.2022
+*/
+
+var outputEl =
+  document.getElementById("output");
+  document.body.appendChild(outputEl);
+
+  var new1El =
+  document.createElement("p");
+  new1El.id = "output";
+  new1El.innerHTML = "Hello!!";
+  document.body.appendChild(new1El);
+
+  var new2El =
+  document.createElement("p");
+  new2El.id = "output";
+  new2El.innerHTML = "This is my second element!?!?!";
+  document.body.appendChild(new2El);
